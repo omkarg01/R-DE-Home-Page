ad718b89519e097789ab78c127ee9c5aa771150f
omkarg01 <omkargujja01@gmail.com>
2022-08-18 14:48:27 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        // 'gold': '#AC9870',
+        // 'blue': '#000e7b'
+      }
+    },
+  },
+  plugins: [],
+}
